2a58d9498737fb7b8e88f319e5686e122e5bd698
tzs <377603002@qq.com>
2024-04-11 14:57:18 +0800

    添加注解驱动方式

---
+++ rpc-annotation/src/main/java/com/zest/annotation/EnableRpc.java
@@ -0,0 +1,9 @@
+package com.zest.annotation;/**
+* @author Zest7
+* @Email tanzhangshun@163.com
+* @github https://github.com/Zest7
+* @date 2024/4/10 
+* @Description: TODO
+*/
+public interface EnableRpc {
+}
